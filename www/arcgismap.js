baf45c4f02ca2268633517f5123f625a1d84becb
siennaatsafe <39389623+siennaatsafe@users.noreply.github.com>
2018-11-19 16:24:26 -0800

    UpdatedDataStreaming

--- www/arcgismap.js
+++ www/arcgismap.js
@@ -1,4 +1,4 @@
-var map, toolbar, clippingGeometry;
+var map, toolbar, clippingGeometry, loading, layer;
 
 
 window.onload = function() {
@@ -15,11 +15,13 @@ window.onload = function() {
  ) {
   map = new Map("mapDiv", {
    basemap: "hybrid",
-   center: [-3.45, 51.99],
-   zoom: 8,
+   center: [-3.45, 52.2],
+   zoom: 9,
    minZoom: 6,
    smartNavigation: false
   });
+  loading = dojo.byId("loadingImg");
+  esri.hide(loading);
 
   map.on("load", function() {
    toolbar = new Draw(map);
@@ -45,6 +47,7 @@ window.onload = function() {
    map.graphics.add(graphic);
    toolbar.deactivate(Draw.POLYGON);
    clippingGeometry = geometry.rings[0];
+   console.log(clippingGeometry);
 
   }
 
@@ -66,10 +69,169 @@ window.onload = function() {
  var repository = "Wales";
  var workspace = "WalesDataDistribution.fmw";
  FMEServer.getWorkspaceParameters(repository, workspace, generateForm);
+}
+// Show Loading Icon
+function showLoading() {
+    esri.show(loading);
+    map.disableMapNavigation();
+    map.hideZoomSlider();
 
 
+}
 
-};
+// Hide Loading Icon
+function hideLoading(error) {
+    esri.hide(loading);
+    map.enableMapNavigation();
+    map.showZoomSlider();
+
+}
+function seaCaves() {
+var seaCaves = document.getElementById("seacavesLyr");
+if (seaCaves.checked == true) {
+
+var workspace = "https://demos-safe-software.fmecloud.com/fmedatastreaming/Wales/WalesDataStreaming.fmw?tested&FEATURE_TYPES=NRW_ART17_SEA_CAVE";
+          loadData(workspace)
+
+          function loadData(workspace) {
+              require([
+                  "esri/layers/KMLLayer", "dojo/parser"
+              ], function (
+                  KMLLayer, parser
+              ) {
+
+
+                  parser.parse();
+                  // Create the ArcGIS Layer and display on the map
+                  showLoading();
+                  seacaveslayer = new KMLLayer(workspace);
+                  map.addLayer(seacaveslayer, 1000);
+                  seacaveslayer.on("load", function () {
+                  hideLoading();
+                  });
+              });
+          }
+    } else {
+          map.removeLayer(seacaveslayer);
+      }
+}
+function estuaries() {
+var estuaries = document.getElementById("estuariesLyr");
+if (estuaries.checked == true) {
+
+var workspace = "https://demos-safe-software.fmecloud.com/fmedatastreaming/Wales/WalesDataStreaming.fmw?FEATURE_TYPES=NRW_ART17_ESTUARIES";
+          loadData(workspace)
+
+          function loadData(workspace) {
+              require([
+                  "esri/layers/KMLLayer", "dojo/parser"
+              ], function (
+                  KMLLayer, parser
+              ) {
+
+
+                  parser.parse();
+                  // Create the ArcGIS Layer and display on the map
+                  showLoading();
+                  estuarieslayer = new KMLLayer(workspace);
+                  map.addLayer(estuarieslayer, 1000);
+                  estuarieslayer.on("load", function () {
+                  hideLoading();
+                  });
+              });
+          }
+    } else {
+          map.removeLayer(estuarieslayer);
+      }
+}
+function floodmaps() {
+var floodmap = document.getElementById("floodmapLyr");
+if (floodmap.checked == true) {
+
+var workspace = "https://demos-safe-software.fmecloud.com/fmedatastreaming/Wales/WalesDataStreaming.fmw?FEATURE_TYPES=NRW_HISTORIC_FLOODMAP";
+          loadData(workspace)
+
+          function loadData(workspace) {
+              require([
+                  "esri/layers/KMLLayer", "dojo/parser"
+              ], function (
+                  KMLLayer, parser
+              ) {
+
+
+                  parser.parse();
+                  // Create the ArcGIS Layer and display on the map
+                  showLoading();
+                  floodmaplayer = new KMLLayer(workspace);
+                  map.addLayer(floodmaplayer, 1000);
+                  floodmaplayer.on("load", function () {
+                  hideLoading();
+                  });
+              });
+          }
+    } else {
+          map.removeLayer(floodmaplayer);
+      }
+}
+function Coastalpath() {
+var coastal = document.getElementById("walescoastalpathLyr");
+if (coastal.checked == true) {
+
+var workspace = "https://demos-safe-software.fmecloud.com/fmedatastreaming/Wales/WalesDataStreaming.fmw?FEATURE_TYPES=NRW_WALES_COASTAL_PATH";
+          loadData(workspace)
+
+          function loadData(workspace) {
+              require([
+                  "esri/layers/KMLLayer", "dojo/parser"
+              ], function (
+                  KMLLayer, parser
+              ) {
+
+
+                  parser.parse();
+                  // Create the ArcGIS Layer and display on the map
+                  showLoading();
+                  coastallayer = new KMLLayer(workspace);
+                  map.addLayer(coastallayer, 1000);
+                  coastallayer.on("load", function () {
+                  hideLoading();
+                  });
+              });
+          }
+    } else {
+          map.removeLayer(coastallayer);
+      }
+}
+function marineChar() {
+var marine = document.getElementById("marinecharacterLyr");
+if (marine.checked == true) {
+
+var workspace = "https://demos-safe-software.fmecloud.com/fmedatastreaming/Wales/WalesDataStreaming.fmw?FEATURE_TYPES=NRW_MARINE_CHARACTER_AREAS";
+          loadData(workspace)
+
+          function loadData(workspace) {
+              require([
+                  "esri/layers/KMLLayer", "dojo/parser"
+              ], function (
+                  KMLLayer, parser
+              ) {
+
+
+                  parser.parse();
+                  // Create the ArcGIS Layer and display on the map
+                  showLoading();
+                  marinelayer = new KMLLayer(workspace);
+                  map.addLayer(marinelayer, 1000);
+                  marinelayer.on("load", function () {
+                  hideLoading();
+                  });
+              });
+          }
+    } else {
+          map.removeLayer(marinelayer);
+      }
+}
+
 
 function generateForm(json) {
  var form = document.getElementById("output-form");
@@ -83,7 +245,11 @@ function generateForm(json) {
    list[i].parentElement.removeChild(list[i]);
 
 
-
+ // Remove the auto generated Feature_Types element and label
+ var flist = document.getElementsByClassName("FEATURE_TYPES");
+ for (var i = flist.length - 1; 0 <= i; i--)
+ if (flist[i] && flist[i].parentElement)
+ flist[i].parentElement.removeChild(flist[i]);
 
 
  // Create the Run Data Download Button
@@ -108,7 +274,7 @@ function showResults(json) {
  console.log(json.serviceResponse);
 
  var downloadarea = document.getElementById("downloadarea");
- 
+
 
  if (numOutput != 0){
          downloadarea.innerHTML = "";
@@ -118,13 +284,6 @@ function showResults(json) {
         downloadarea.innerHTML = "";
 		 downloadarea.innerHTML += "No features found in selected area. Please try again.";
        }
-     
-     
-     
- 
- 
-
-
 
 }
 
@@ -137,6 +296,18 @@ function processClip() {
  var form = document.getElementById("output-form");
  var downloadarea = document.getElementById("downloadarea");
  var requestarea = document.getElementById("requestarea");
+ var layerform = document.getElementById("exampleForm");
+
+ //See the layers the user selected
+ // Loop through unique parameters and build the parameter string
+ for (var i = 0; i < layerform.length; i++) {
+  var element = layerform.elements[i];
+   if (element.type == "checkbox") {
+   if (element.checked) {
+    params += element.name + "=" + element.value + "&";
+   }
+  } 
+ }
 
 
  downloadarea.innerHTML = "<p>Processing Data...</p>";
@@ -150,12 +321,14 @@ function processClip() {
    if (element.checked) {
     params += element.name + "=" + element.value + "&";
    }
+  } else if (element.type == "button") { 
+
   } else {
    params += element.name + "=" + element.value + "&";
   }
  }
  // Remove trailing & from string
- params = params.substr(0, params.length - 19);
+ params = params.substr(0, params.length - 1);
 
  // Process the clippingGeometry into a WKT Polygon string
  var geometry = "POLYGON((";
@@ -171,7 +344,7 @@ function processClip() {
  geometry += "))";
 
 
- params += "GEOM=" + geometry;
+ params += "&GEOM=" + geometry;
 
 
  var str = '';
